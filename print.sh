ff78e6f9302124e17c6f2f76f9d0f4a2e76c51b7
Akhil p <akhilposhala09@gmail.com>
2022-06-13 21:12:30 +0530

    printing the text with colour

--- print.sh
+++ print.sh
@@ -4,3 +4,18 @@ echo Hello World
 
 #Multi line printing
 #syntax : echo -e "Line1\nLine2"
+
+#printing with colours
+#colour codes
+#31 RED
+#32 GREEN
+#33 YELLOW
+#34 BLUE
+#35 MAGENTA
+#36 CYAN
+#0 RESET
+
+#syntax : echo -e "\e[COLMessage\e[0m"
+
+echo -e "\e[31mRED TExt
+echo Normal text"
