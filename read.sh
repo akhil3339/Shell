4a177371b348e63e4f88899610880a8332fb6264
Akhil p <akhilposhala09@gmail.com>
2022-06-13 23:11:26 +0530

    reading the arguments

---
+++ read.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+Special variables are $0-$n, $*, $@, $#
+
+echo first argument =$1
+echo second argument =$2
+
+#future information
+echo script name=$0
+
+echo all arguments =$*
+
+echo no of arguments =$#
